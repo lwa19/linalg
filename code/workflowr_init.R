9a31304d5f10097e4e5626684441b5471e3fbae0
Lijia Wang <lwa19@users.noreply.github.com>
2020-06-24 21:57:40 -0500

    create ch1 rmd file

---
+++ code/workflowr_init.R
@@ -0,0 +1,24 @@
+## setting up workflowr for linalg
+library(workflowr)
+
+## setup the repository
+setwd("~/Documents/GitHub/")
+wflow_git_config(user.name = "Lijia Wang", user.email = "lwang19@uchicago.edu")
+wflow_start("linalg", existing = TRUE)
+
+## initialize the build of the website
+wflow_build()
+# wflow_view()
+wflow_status()
+setwd("~/Documents/GitHub/linalg/analysis/")
+wflow_publish(c("index.Rmd", "about.Rmd", "license.Rmd"),
+              "Publish the initial files for myproject")
+wflow_use_github("lwa19")
+wflow_git_push()
+
+## subsequent pushes need to run:
+wflow_publish()
+wflow_git_push()
+
+# initialize new Rmd files: 
+wflow_open("chapter1.Rmd")
