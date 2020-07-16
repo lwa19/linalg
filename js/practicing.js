70ced82b9bdece1b62e59b30875bccfd3f44663e
smarglous <67377474+smarglous@users.noreply.github.com>
2020-07-16 17:05:19 -0400

    update

---
+++ js/practicing.js
@@ -0,0 +1,12 @@
+(function($) {
+    $(document).ready(function() {
+	
+	$('#practicing').scianimator({
+	    'images': ['images/practicing1.png', 'images/practicing2.png', 'images/practicing3.png', 'images/practicing4.png', 'images/practicing5.png', 'images/practicing6.png', 'images/practicing7.png', 'images/practicing8.png', 'images/practicing9.png', 'images/practicing10.png', 'images/practicing11.png', 'images/practicing12.png', 'images/practicing13.png', 'images/practicing14.png', 'images/practicing15.png', 'images/practicing16.png', 'images/practicing17.png', 'images/practicing18.png', 'images/practicing19.png', 'images/practicing20.png', 'images/practicing21.png'],
+	    'width': 480,
+	    'delay': 50,
+	    'loopMode': 'loop'
+	});
+	$('#practicing').scianimator('play');
+    });
+})(jQuery);
